f5a06bca3da7d1e25489304b6ed931e7dac41fb0
Ravil Shabaykin <rr.shabaykin@outlook.com>
2023-03-05 22:44:36 +0300

    Update relations

--- relation.py
+++ relation.py
@@ -13,15 +13,13 @@ def find_relation(pattern: str):
     t_col = r'\(t\)'
     for (M1, N1), (M2, N2) in zip(keys, keys[1:]):
         print(M2, N2, '--', M1, N1)
-        df = (dfs[(M2, N2)] / dfs[(M1, N1)]).round(4)
-        df[t_col] = dfs[(M1, N1)][t_col]
+        df = dfs[(M2, N2)] - dfs[(M1, N1)]
         out.append(df)
-    for df in out[1:]:
-        for col in df.columns:
-            if col == t_col: continue
-            out[0][col] = out[0][col].astype(str) + ';' + df[col].astype(str)
-    out[0].rename(columns=lambda x: x.replace('u', r"\frac{u_{MN}}{u_{M'N'}}"))
-    out[0].to_csv(f'rel_{pattern}.csv', index=False)
+    assert len(out) == 2
+    df = out[0] / out[1]
+    df[t_col] = dfs[keys[0]][t_col]
+    df.rename(columns=lambda x: x.replace(r'\left.u\right\vert_', ''), inplace=True)
+    df.round(6).astype(str).to_csv(f'rel_{pattern}.csv', index=False)
 
 
 find_relation('a0.1_b1.0')
